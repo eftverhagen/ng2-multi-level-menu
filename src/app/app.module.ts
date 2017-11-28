7c3f7d724fe1b019ef01f5eca0f68a31c293cf0b
= <eftverhagen@gmail.com>
2017-11-28 17:51:48 +0100

    Create header component with minimal styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { MultiLevelMenuComponent } from './multi-level-menu/multi-level-menu.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MultiLevelMenuComponent
+    MultiLevelMenuComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
