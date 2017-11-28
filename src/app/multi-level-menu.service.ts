8f3b4ad967e38d2d29376eb12c04d9bc9ef90b3f
= <eftverhagen@gmail.com>
2017-11-28 19:58:44 +0100

    Few steps further

--- src/app/multi-level-menu.service.ts
+++ src/app/multi-level-menu.service.ts
@@ -4,12 +4,14 @@ import * as _ from 'lodash';
 @Injectable()
 export class MultiLevelMenuService {
   private state;
+  private initialWidth;
   private w;
   private contentLeft;
 
   constructor() { 
-    this.state = [0, 0, 0];
-    this.w = [340];
+    this.state = [0, 0];
+    this.initialWidth = 300;
+    this.w = [300];
   }
 
   calculateContentLeft(){
@@ -19,14 +21,29 @@ export class MultiLevelMenuService {
 
   calculateLevelSize(){
     let levelsOpen = _.sumBy(this.state, i => i === true ? 1 : 0);
+    let margin = levelsOpen;
+    this.w = _.map(this.state, (w, i) => {
+      let size = this.initialWidth + margin * 40;
+      margin -= 1;
+      return size;
+    });
+    console.log('width', this.w);
     console.log('levels open:', levelsOpen);
   }
 
   toggle(level) {
-    let idx = level || 0;
-    this.state[idx] = !this.state[idx];
-    this.calculateContentLeft();
+    if(level){
+      this.state[level] = !this.state[level];
+    }else{
+      this.state = _.map(this.state, (l, i) => {
+        return i === 0 ? !l : 0;
+      });
+
+    }
+  
     this.calculateLevelSize();
+    this.calculateContentLeft();
+    console.log(this.state);
     
   }
 
