5cf636f9aee4552c5b8cf41fcd3acac2f2cd1911
= <eftverhagen@gmail.com>
2017-11-28 18:14:33 +0100

    Let content translate3d as well

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { MultiLevelMenuService } from './multi-level-menu.service';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  constructor(private _menu: MultiLevelMenuService) {
+    
+  }
   title = 'app';
 }
