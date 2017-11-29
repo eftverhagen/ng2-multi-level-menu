1dd177951843d4e7f2ed16c92f573c325cec9931
= <eftverhagen@gmail.com>
2017-11-29 22:02:09 +0100

    Satisfied about structure

--- src/app/shared/menu-structure.ts
+++ src/app/shared/menu-structure.ts
@@ -1,27 +1,15 @@
-// export const menuStructure = [
-//     [{name: 'Cars'},{name: 'Devices'}, {name: 'Clothing'}], // 0  : 0, 1
-//     [{name: 'Audi'},{name: 'Toyota', link: '/toyota'}], // 1 : 0, 1
-//     [{name: 'Laptop'},{name: 'Tablet'}], // 2 : 0, 1
-//     [{name: 'Jeans'},{name: 'Sweater'}] // 3 : 0, 1
-// ];
-
-
 export const menuStructure = [
     {id: 0, items: [ // Level 1
         {name: 'Cars', toggle: 1}, // Toggle level 2 (Toyota)
+        {name: 'Clothing', toggle: 2}, // Toggle level 2 (Toyota)
     ]}, // link
     {id: 1, items: [ // Level 2
         {name: 'Aygo', route: 'Aygo'}, // Toggle level 2 (Toyota)
-        {name: 'Prius', route: 'Prius'}]}, // link
-];
+        {name: 'Prius', route: 'Prius'}
+    ]}, // link
+    {id: 2, items: [ // Level 3
+        {name: 'Jeans', route: 'Jeans'}, // Toggle level 2 (Toyota)
+        {name: 'Sweater', route: 'Sweater'} // link
+    ]}
 
-// Level 1
-//  - toggle Level 2
-//  - link
-// Level 2
-//  - toggle Level 3
-//  - link
-//  - link
-// Level 3
-//  - link
-//  - link
+];
